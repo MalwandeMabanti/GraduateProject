a7bbc933ce6a0b49b81f543530847b697a09ef72
Malwande Mabanti <malwandem@payspace.com>
2023-03-06 12:02:11 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Newroject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
